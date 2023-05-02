01c4528731887867c04ae3418d2d4efe06e51b3a
Oscar <oscar@claimstrak.co.uk>
2023-05-02 14:23:51 +0545

    section:5/ video 68 => 'Adding a CHART'

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
   // yo parameter le ExpenseForm bata data receive garcha,parameter ko name j rakhda ni huncha
+
+  const [isEditing, setIsEditing] = useState(false); //(Assignment-4) yesle chai FORM shoe garne ki ma garne bhanne decide garcha
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     //ExpenseForm.JS ko submit handler ma data kasari receive gareko cha yaha pani tei same name ra way use gareko ho, yo chai OBJECT banako ho
     const expenseData = {
@@ -13,10 +16,32 @@ const NewExpense = (props) => {
 
     // APP.JS bata aako ho//yesle NewExpense.js bata aako data lai APP.JS ma pass garcha
     props.onAddExpense(expenseData);
+    setIsEditing(false); //--form submit bhaye pachi form lai close garna lai
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  // -- stopEditingHandler() lai ExpenseForm.js ko cancel button ma pass gariyeko cha
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {/* if isEditing FALSE cha bhane BUTTON show garne */}
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+
+      {/* If isEditing TRUE cha bhane FORM show garne */}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
