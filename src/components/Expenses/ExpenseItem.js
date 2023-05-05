ad090a1b647e30aac4cc8faf222d5f7eb7489dc0
Oscar <oscar@claimstrak.co.uk>
2023-05-05 11:25:53 +0545

    section:5/ video 69 => 'Adding dynamic styles: CHART BARS'

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+// import { useState } from 'react';
+import React from 'react';
 import ExpenseDate from '../Expenses/ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
   // const clickHandler = () => {
   //   setTitle('Updated!');
@@ -16,7 +17,7 @@ const ExpenseItem = (props) => {
       <Card className='expense-item'>
         <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
           {/* <button onClick={clickHandler}>Change Title</button> */}
         </div>
